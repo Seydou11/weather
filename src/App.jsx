90fb0141baf4ffb14ada2b7c6bd37ef5d2ca1474
Seydou11 <koneseydoudro@gmail.com>
2026-05-07 12:02:06 -0400

    feat: affichage température
    afficher les température par ville en fonctionne du pays.

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import loader from "./assets/loader.svg";
+import browser from "./assets/browser.svg";
+import "./App.css";
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
+function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [errorInfo, setErrorInfo] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://api.airvisual.com/v2/nearest_city?key=${API_KEY}`)
+      .then((response) => {
+        // 400-499: error from the client,
+        // 500-599: error from the server
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}, ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setWeatherData({
+          city: data.data.city,
+          country: data.data.country,
+          temperature: data.data.current.weather.tp,
+          weatherIcon: data.data.current.weather.ic,
+        });
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+        setErrorInfo(error.message);
+      });
+  }, []);
+
+  return (
+    <main>
+      <div
+        className={`loader-container ${!weatherData && !errorInfo && "active"}`}
+      >
+        <img src={loader} alt="loading icon" />
+      </div>
+      {weatherData && (
+        <>
+          <p className="city-name">{weatherData?.city}</p>
+          <p className="country-name">{weatherData?.country}</p>
+          <p className="temperature">{weatherData?.temperature}</p>
+          <div className="info-icon-container">
+            <img
+              src={`/icons/${weatherData?.weatherIcon}.svg`}
+              className="info-icon"
+              alt="weather icon"
+            />
+          </div>
+        </>
+      )}
+
+      {errorInfo && !weatherData && (
+        <>
+          <p className="error-message">{errorInfo}</p>
+          <img src={browser} alt="error icon" />
+        </>
+      )}
+    </main>
+  );
+}
+
+export default App;
